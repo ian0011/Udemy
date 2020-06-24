e77c37056e1861809f765638cc23e9168d8bf474
Ian Victor <ianmoura607@gmail.com>
2020-06-24 20:57:38 -0300

    Segundo Problema
    Inserção de um produto com nome, valor e quantidade

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,45 +1,32 @@
 package application;
 
-import entities.Triangle;
+import entities.Product;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
-        //Instanciando o Java Scanner
+        
         Locale.setDefault(Locale.US);
+        //Instanciando o Java Scanner
         Scanner sc = new Scanner(System.in);
         //aqui ficavam as variáveis  an
-        Triangle x,y;
-        //instanciando o objeto
-        x = new Triangle();
-        y = new Triangle();
-         
-
-        System.out.println("Digite as medidas do triângulo X:");
-        x.a = sc.nextDouble();
-        x.b = sc.nextDouble();
-        x.c = sc.nextDouble();
-        System.out.println("Digite as medidas do triângulo Y:");
-        y.a = sc.nextDouble();
-        y.b = sc.nextDouble();
-        y.c = sc.nextDouble();
         
-        //chamada do metodo
-        double areaX = x.area();
+        Product product = new Product();
+        System.out.println("Enter product data:");
+        System.out.println("Nome: ");
+        product.name = sc.nextLine();
+        System.out.print("Price: ");
+        product.price = sc.nextDouble();
+        System.out.println("Quantity in stock: ");
+        product.quantity = sc.nextInt();
         
-        //chamada do metodo
-        double areaY = y.area();
-        System.out.printf("Triângulo x area: %.4f%n", areaX);
-        System.out.printf("Triângulo y area: %.4f%n", areaY);
+        System.out.println(product.name + ", " + product.price + "," + product.quantity);
         
-        if(areaX > areaY){
-            System.out.println("Area largura: X");
-        }else{
-            System.out.println("Area largura: Y");
-        }
-       
+        
+        
+        
         sc.close();
         
     }
