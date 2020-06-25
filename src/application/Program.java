a377d4133af9b59fcac0e729f1e82f17e6b5a78c
Ian Victor <ianmoura607@gmail.com>
2020-06-24 21:59:40 -0300

    Object e toString
    Entendendo objeto e a utilização do toString.

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,12 +7,12 @@ import java.util.Scanner;
 public class Program {
 
     public static void main(String[] args) {
-        
+
         Locale.setDefault(Locale.US);
         //Instanciando o Java Scanner
         Scanner sc = new Scanner(System.in);
         //aqui ficavam as variáveis  an
-        
+
         Product product = new Product();
         System.out.println("Enter product data:");
         System.out.println("Nome: ");
@@ -21,13 +21,10 @@ public class Program {
         product.price = sc.nextDouble();
         System.out.println("Quantity in stock: ");
         product.quantity = sc.nextInt();
-        
-        System.out.println(product.name + ", " + product.price + "," + product.quantity);
-        
-        
-        
-        
+
+        System.out.println(product.toString());
+
         sc.close();
-        
+
     }
 }
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -1,23 +1,31 @@
-
 package entities;
 
-
 public class Product {
+
     public String name;
     public double price;
     public int quantity;
-    
-    
-    public double totalValueInStock(){
+
+    public double totalValueInStock() {
         return price * quantity;
     }
-    
-    public void addProducts(int quantity){
+
+    public void addProducts(int quantity) {
         this.quantity += quantity;
     }
-   
-    public void removeProducts(int quantity){
+
+    public void removeProducts(int quantity) {
         this.quantity -= quantity;
     }
-    
+
+    public String toString() {
+        return name + ", $ "
+                + price
+                + ", "
+                + quantity
+                + " units, Total: $ "
+                + totalValueInStock();
+
+    }
+
 }
