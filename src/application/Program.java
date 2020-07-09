c660bd46021d174e9c5c6c5b3bfe8e3a30b229da
Ian Victor <ianmoura607@gmail.com>
2020-07-09 12:26:43 -0300

    Vetor
    Inserção dos vetores e utilização do vect.length para verificar o tamanho

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,46 +1,37 @@
+
 package application;
 
-import entities.Triangle;
+import entities.Product;
 import java.util.Locale;
 import java.util.Scanner;
 
+/**
+ @author Ian Victor
+ */
 public class Program {
-
-    public static void main(String[] args) {
-        //Instanciando o Java Scanner
+    public static void main(String[] args){
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        //aqui ficavam as variáveis  an
-        Triangle x,y;
-        //instanciando o objeto
-        x = new Triangle();
-        y = new Triangle();
-         
-
-        System.out.println("Digite as medidas do triângulo X:");
-        x.a = sc.nextDouble();
-        x.b = sc.nextDouble();
-        x.c = sc.nextDouble();
-        System.out.println("Digite as medidas do triângulo Y:");
-        y.a = sc.nextDouble();
-        y.b = sc.nextDouble();
-        y.c = sc.nextDouble();
         
-        //chamada do metodo
-        double areaX = x.area();
+        int n = sc.nextInt();
+        Product[] vect = new Product[n];
         
-        //chamada do metodo
-        double areaY = y.area();
-        System.out.printf("Triângulo x area: %.4f%n", areaX);
-        System.out.printf("Triângulo y area: %.4f%n", areaY);
-        
-        if(areaX > areaY){
-            System.out.println("Area largura: X");
-        }else{
-            System.out.println("Area largura: Y");
+        for (int i = 0; i < vect.length; i++) {
+            sc.nextLine();
+            String name = sc.nextLine();
+            double price = sc.nextDouble();
+            vect[i] = new Product (name, price);
         }
-       
-        sc.close();
+        double sum = 0.0;
+        for (int i = 0; i < vect.length; i++) {
+            sum += vect[i].getPrice();
+        }
         
+        double avg = sum / vect.length;
+        
+        System.out.printf("AVERAGE PRICE = %.2f%n", avg);
+        
+        
+        sc.close();
     }
 }
