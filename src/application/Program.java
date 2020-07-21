f0a0c4075c5ec384be3f4fca2d6882d3070ead41
Ian Victor <ianmoura607@gmail.com>
2020-07-21 13:12:00 -0300

    Upcasting & Downcasting

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,60 +1,39 @@
-
 package application;
 
-import entities.Department;
-import entities.HourContract;
-import entities.Worker;
-import entities.enums.WorkerLevel;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.Scanner;
-
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
 
 public class Program {
-   
-    public static void main(String[] args) throws ParseException{
+
+    public static void main(String[] args)  {
+        Account acc = new Account(1001,"Ian",0.0);
+        BusinessAccount bacc = new BusinessAccount(1002, "Victor", 0.0, 500.0);
         
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         
-        System.out.print("Entre com o nome do departamento: ");
-        String nomeDepartamento = sc.nextLine();
-        System.out.println("Insira dados do trabalhador:");
-        System.out.print("Nome:");
-        String nomeTrabalhador = sc.nextLine();
-        System.out.print("Cargo:");
-        String cargoTrabalhador = sc.nextLine();
-        System.out.print("Salário Base:");
-        double salarioTrabalhador = sc.nextDouble();
+        //UPCASTING
         
-        Worker worker = new Worker (nomeTrabalhador, WorkerLevel.valueOf(cargoTrabalhador), salarioTrabalhador,new Department(nomeDepartamento));
+        Account acc1 = bacc;
+        Account acc2 = new BusinessAccount(1003,"João", 0.0,0.01);
+       
         
-        System.out.print("Quantos contratos o trabalhador tem?");
-        int n = sc.nextInt();
-        for (int i = 1; i < n; i++) {
-            System.out.println("Entre com o contrato #"+ i + " data:");
-            System.out.print("Data (DD/MM/YYYY): ");
-            Date dataContrato = sdf.parse(sc.next());
-            System.out.print("Valor por hora:");
-            double valorPorHora = sc.nextDouble();
-            System.out.print("Duração em horas");
-            int hours = sc.nextInt();
-            HourContract contrato = new HourContract(dataContrato,valorPorHora,hours);
-            worker.addContract(contrato);
+        //DOWNCASTING nem sempre vai dá certo, o compilador só vai avisar quando executar
+        BusinessAccount acc4 = (BusinessAccount)acc2;
+        acc4.loan(100.0);
+        
+        //BusinessAccount acc5 = (BusinessAccount) acc3;
+        
+        if(acc2 instanceof BusinessAccount){
+            BusinessAccount acc5 = (BusinessAccount)acc2;
+            acc5.loan(200.0);
+            System.out.println("Empréstimo");
+        }
+        if(acc2 instanceof SavingsAccount){
+            SavingsAccount acc5 = (SavingsAccount)acc2;
+            acc5.updateBalance();
+            System.out.println("atualizado");
         }
         
-        System.out.println();
-        System.out.print("Entre com o mês e o ano (MM/YY): ");
-        String mes_Ano = sc.next();
-        int mes = Integer.parseInt(mes_Ano.substring(0,2));
-        int ano = Integer.parseInt(mes_Ano.substring(3));
-        System.out.println("Name: " + worker.getName());
-        System.out.println("Departamento: " + worker.getDepartment().getName());
-        System.out.println("Renda " + mes_Ano + ": " + String.format("%.2f", worker.income(mes, ano)));
         
-        sc.close();
     }
 }
--- src/entities/BusinessAccount.java
+++ src/entities/BusinessAccount.java
@@ -1,15 +1,15 @@
 package entities;
 
-public class BusinessAccount extends Account {
-
+public class BusinessAccount extends Account{
+    
     private Double loanLimit;
-
+    
     public BusinessAccount() {
-        super();
+     super();
     }
-
-    public BusinessAccount(Double loanLimit, Integer number, String holder, Double balance) {
-        super(number, holder, balance);
+    
+    public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit){
+        super(number,holder,balance);
         this.loanLimit = loanLimit;
     }
 
@@ -20,11 +20,10 @@ public class BusinessAccount extends Account {
     public void setLoanLimit(Double loanLimit) {
         this.loanLimit = loanLimit;
     }
-
-    public void loan(double amount) {
-        if (amount <= loanLimit) {
-            balance += amount - 10.0;
+    
+    public void loan(double amount){
+        if(amount <= loanLimit){
+        balance += amount - 10.0;
         }
     }
-
 }
