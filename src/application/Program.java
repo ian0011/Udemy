0002dee9ee29ee036ddc896bc0cbeb0b5ba9d507
Ian Victor <ianmoura607@gmail.com>
2020-07-27 19:49:22 -0300

    Classes Abstratas

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,23 +3,30 @@ package application;
 import entities.Account;
 import entities.BusinessAccount;
 import entities.SavingsAccount;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Program{
-    public static void main(String[] args){
-        
-        Account acc1 = new Account(1001,"Alex",1000.0);
-        acc1.withdraw(200.0);
-        System.out.println(acc1.getBalance());
-        
-        Account acc2 = new SavingsAccount(1002,"Maria",1000.0);
-        acc2.withdraw(200.0);
-        System.out.println(acc2.getBalance());
-        
-        Account acc3 = new BusinessAccount(1003,"Ian",1000.0,500.0);
-        acc3.withdraw(200.0);
-        System.out.println(acc3.getBalance());
-        
-        
-        
+public class Program {
+
+    public static void main(String[] args) {
+
+        List<Account> list = new ArrayList<>();
+        list.add(new SavingsAccount(1001, "alex", 500.0, 0.01));
+        list.add(new BusinessAccount(1002, "Maria", 1000.0, 400.0));
+        list.add(new SavingsAccount(1003, "Jorge", 1500.0, 0.01));
+        list.add(new BusinessAccount(1005, "Marcos", 1500.0, 500.0));
+
+        double soma = 0.0;
+        for (Account acc : list) {
+            soma += acc.getBalance();
+        }
+        System.out.printf("Total balance: %.2f%n", soma);
+
+        for (Account acc : list) {
+            acc.deposit(10.0);
+        }
+        for (Account acc : list){
+            System.out.println(acc.getBalance());
+        }
     }
 }
--- src/entities/SavingsAccount.java
+++ src/entities/SavingsAccount.java
@@ -1,31 +1,32 @@
 package entities;
 
 public class SavingsAccount extends Account {
+	
+	private Double interestRate;
+	
+	public SavingsAccount() {
+		super();
+	}
 
-    private Double interestRate;
+	public SavingsAccount(Integer number, String holder, Double balance, Double interestRate) {
+		super(number, holder, balance);
+		this.interestRate = interestRate;
+	}
 
-    public SavingsAccount() {
-        super();
-    }
+	public Double getInterestRate() {
+		return interestRate;
+	}
 
-    public SavingsAccount(Integer number, String holder, Double balance) {
-        super(number, holder, balance);
-        this.interestRate = interestRate;
-    }
-
-    public Double getInterestRate() {
-        return interestRate;
-    }
-
-    public void setInterestRate(Double interestRate) {
-        this.interestRate = interestRate;
-    }
-
-    public void updateBalance() {
-        balance += balance * interestRate;
-    }
-    @Override
-    public void withdraw(double amount){
-        balance -= amount;
-    }
+	public void setInterestRate(Double interestRate) {
+		this.interestRate = interestRate;
+	}
+	
+	public void updateBalance() {
+		balance += balance * interestRate;
+	}
+	
+	@Override
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
 }
