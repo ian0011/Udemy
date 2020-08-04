327908e24030ea011871f4498632889fa84d26b6
Ian Victor <ianmoura607@gmail.com>
2020-08-04 13:05:38 -0300

    Stack Trace
    Pilha de chamada de métodos

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,23 +3,39 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-
 public class Program {
-    public static void main (String [] args){
+
+    public static void main(String[] args) {
+
+        method1();
+
+        System.out.println("Fim do programa");
+
+    }
+
+    public static void method1() {
+        System.out.println("***METODO 1 START***");
+        method2();
+        System.out.println("***Method 1 END***");
+    }
+
+    public static void method2() {
         Scanner sc = new Scanner(System.in);
-        
+        System.out.println("***METODO 2 START***");
+
         try {
-        String[] vect = sc.nextLine().split(" ");
-        int position = sc.nextInt();
-        System.out.println(vect[position]);
-        }
-        catch(ArrayIndexOutOfBoundsException e){
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Invalid position!");
-        }
-        catch(InputMismatchException e){
+            e.printStackTrace();
+            sc.next();
+        } catch (InputMismatchException e) {
             System.out.println("Input error!");
         }
-        System.out.println("Fim do programa");
+        System.out.println("***Method 2 END***");
         sc.close();
     }
+
 }
