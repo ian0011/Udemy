d8eeac0007fe032be589f7f683c0b29565fca171
Ian Victor <ianmoura607@gmail.com>
2020-08-06 12:42:26 -0300

    Exemplo try catch

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,41 +1,51 @@
 package application;
 
-import java.util.InputMismatchException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
+
+import model.entities.Reservation;
+import model.exceptions.DomainException;
 
 public class Program {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        method1();
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		try {
+			System.out.print("Room number: ");
+			int number = sc.nextInt();
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			Date checkIn = sdf.parse(sc.next());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			Date checkOut = sdf.parse(sc.next());
+			
+			Reservation reservation = new Reservation(number, checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+			
+			System.out.println();
+			System.out.println("Enter data to update the reservation:");
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			checkIn = sdf.parse(sc.next());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			checkOut = sdf.parse(sc.next());
+			
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+		}
+		catch (ParseException e) {
+			System.out.println("Invalid date format");
+		}
+		catch (DomainException e) {
+			System.out.println("Error in reservation: " + e.getMessage());
+		}
+		catch (RuntimeException e) {
+			System.out.println("Unexpected error");
+		}
 
-        System.out.println("Fim do programa");
-
-    }
-
-    public static void method1() {
-        System.out.println("***METODO 1 START***");
-        method2();
-        System.out.println("***Method 1 END***");
-    }
-
-    public static void method2() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("***METODO 2 START***");
-
-        try {
-            String[] vect = sc.nextLine().split(" ");
-            int position = sc.nextInt();
-            System.out.println(vect[position]);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Invalid position!");
-            e.printStackTrace();
-            sc.next();
-        } catch (InputMismatchException e) {
-            System.out.println("Input error!");
-        }
-        System.out.println("***Method 2 END***");
-        sc.close();
-    }
-
+		sc.close();
+	}
 }
